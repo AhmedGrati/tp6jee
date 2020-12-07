94a39d7555a8f9ee7e81f933a7a01b8f087420d4
mohamedwaelbenismail <ahmedgrati1999@gmail.com>
2020-12-07 15:31:15 +0100

    fix bug

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -4,17 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity
+@Entity(name = "product")
+@Table(name = "product")
 public class Product {
 
     @Id
--- src/main/java/com/example/demo/repo/ProductRepository.java
+++ src/main/java/com/example/demo/repo/ProductRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductRepository  extends JpaRepository<Product,Long> {
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
     Product findByProdName(String prodName);
 }
